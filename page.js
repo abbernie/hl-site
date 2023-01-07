baaf654e32c5b97855f45652ed85790c3220687e
Andrew <andrewbbernstein@gmail.com>
2023-01-07 09:35:34 +0100

    march tour update

--- page.js
+++ page.js
@@ -2,86 +2,73 @@
 var dates = [
 
       {
-		"date":"November 3",
-		"loc":"Berlin, DE",
-		"venue":"Silent Green",
-	    "link":	"https://ticketorganizer.eu/event/horse-lords-northern-spy"
+		"date":"March 7",
+		"loc":"Savona, IT",
+		"venue":"Raindogs",
+	    "link":	"https://raindogshouse.com/"
 	},
         
 	
         {
-		"date":"November 6",
-		"loc":"Prague, CZ",
-		"venue":"Punctum",
-		"link":"https://punctum.cz/"
+		"date":"March 8",
+		"loc":"Milano, IT",
+		"venue":"Spazio Teatro 89",
+		"link":"https://link.dice.fm/B636f5a6647d"
 	},
 	{
-		"date":"November 8",
-		"loc":"Aarhus, DK",
-		"venue":"Alter @ Kunsthal Aarhus",
-		"link":"https://tikkio.com/tickets/31724-alter-horse-lords"
+		"date":"March 9",
+		"loc":"Foligno",
+		"venue":"Spazio Zut",
+		"link":"https://www.diyticket.it/events/Musica/10275/ufo-23-horse-lords"
 	},
 		{
-		"date":"November 9",
-		"loc":"Copenhagen, DK",
-		"venue":"Valby Kulturhus",
-		"link":"https://billetto.dk/e/horse-lords-us-at-valby-kulturhus-billetter-683993"
+		"date":"March 10",
+		"loc":"Ravenna, IT",
+		"venue":"Bronson",
+		"link":"https://link.dice.fm/l8fcd4e597c2"
 	},
 		{
-		"date":"November 10",
-		"loc":"Odense, DK",
-		"venue":"Momentum",
-		"link":"http://www.place2book.com/da/sw2/sales/qywyd0eru8"
+		"date":"March 11",
+		"loc":"Innsbruck, AT",
+		"venue":"Die Bäckerei",
+		"link":"https://unterton.org/veranstaltungen"
 	},
 		{
-		"date":"November 11",
-		"loc":"Leipzig, DE",
-		"venue":"Transcentury Update",
-		"link":"https://www.tixforgigs.com/Event/32976?af=ozqw0atnumoi&saf=s54498u"
+		"date":"March 12",
+		"loc":"Nürnberg, DE",
+		"venue":"Kantine",
+		"link":"https://www.tixforgigs.com/Event/46442"
 	},
 		{
-		"date":"November 13",
-		"loc":"Utrecht, NL",
-		"venue":"Le Guess Who?",
-		"link":"https://www.leguesswho.nl/tickets"
-	},
-	{
-		"date":"November 15",
-		"loc":"Manchester, UK",
-		"venue":"White Hotel",
-		"link":"https://www.seetickets.com/event/horse-lords/the-white-hotel/2386679"
-	},
-		{
-		"date":"November 16",
-		"loc":"London, UK",
-		"venue":"EFG London Jazz @ Moth Club",
-		"link":"https://songkick.seetickets.com/event/horse-lords/moth-club/2367743?pref1=40487894&aff=id1songkick"
-	},
-		{
-		"date":"November 17",
+		"date":"March 13",
 		"loc":"Paris, FR",
-		"venue":"Gare XP",
-		"link":"https://garexp.org/"
+		"venue":"Point Ephemere",
+		"link":"https://link.dice.fm/dfd58b9f3582"
+	},
+	{
+		"date":"March 15",
+		"loc":"Nantes, FR",
+		"venue":"Pole Etudiant",
+		"link":""
+	},
+		{
+		"date":"March 16",
+		"loc":"Tourcoing, FR",
+		"venue":"Le Grand Mix",
+		"link":"https://legrandmix.com/fr/agenda/horse-lords"
+	},
+		{
+		"date":"March 17",
+		"loc":"Gent, BE",
+		"venue":"De Boer",
+		"link":"https://apps.ticketmatic.com/widgets/vooruit/flow/tickets?event=159396900015&l=nl#!/addtickets"
 	},
 	
 			{
-		"date":"November 18",
-		"loc":"Lyon, FR",
-		"venue":"Le Sonic",
-		"link":"https://yurplan.com/event/HORSE-LORDS-guest-au-Sonic/89523#/"
-	},
-	
-	{
-		"date":"November 19",
-		"loc":"Chaux-Du Fonds, CH",
-		"venue":"Bikini Test",
-		"link":"https://www.bikinitest.ch/"
-	},
-		{
-		"date":"November 20",
-		"loc":"St. Gallen, CH",
-		"venue":"Palace",
-		"link":"https://www.petzi.ch/de/events/49838-palace-horse-lords-us-omni-selassi-ch/"
+		"date":"March 18",
+		"loc":"Brussels, BE",
+		"venue":"Beursschouwbourg",
+		"link":"https://apps.ticketmatic.com/widgets/beursschouwburg/flow/tickets?event=655499502850&l=en#!/addtickets"
 	}
 	
 	
