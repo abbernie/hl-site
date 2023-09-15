ab9a9ce6012666bc1a8851e1a3ee2b17f4b816c2
Andrew <andrewbbernstein@gmail.com>
2023-09-15 11:22:18 +0200

    usa tour

--- page.js
+++ page.js
@@ -48,6 +48,72 @@ var dates = [
 		"loc":"Wels, Austria",
 		"venue":"Music Unlimited",
 		"link":"https://www.waschaecht.at/music-unlimited/de"
+	},
+	{
+		"date":"March 12",
+		"loc":"Minneapolis, MN",
+		"venue":"Icehouse w/ Ka Baird",
+		"link":"https://dice.fm/partner/dice/event/mdx3w?dice_id=1515571&dice_channel=web&dice_tags=organic&dice_campaign=DICE&dice_feature=marketing&_branch_match_id=1153706971361173320&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXz8nMy9ZLyUxO1UvL1U81NrJMs7QwNDVJSwYAtckn%2BCEAAAA%3D"
+	},
+	{
+		"date":"March 13",
+		"loc":"Chicago, IL",
+		"venue":"Empty Bottle w/ Ka Baird",
+		"link":"https://www.ticketweb.com/event/horse-lords-ka-baird-empty-bottle-tickets/13593628?pl=eb"
+	},
+	{
+		"date":"March 14",
+		"loc":"Cleveland, OH",
+		"venue":"Grog Shop w/ Ka Baird",
+		"link":"https://www.ticketweb.com/event/horse-lords-ka-baird-grog-shop-tickets/13438338?pl=grogshop"
+	},
+	{
+		"date":"March 15",
+		"loc":"Toronto, ON",
+		"venue":"Baby G w/ Ka Baird",
+		"link":"https://www.ticketscene.ca/events/45861/"
+	},
+	{
+		"date":"March 16",
+		"loc":"Montréal, PQ",
+		"venue":"La Sotterenea w/ Ka Baird",
+		"link":"https://www.venuepilot.co/events/80657/orders/new"
+	},
+	{
+		"date":"March 17",
+		"loc":"Peterborough, NH",
+		"venue":"Nova Arts w/ Ka Baird",
+		"link":"http://changeoverfestival.com/"
+	},,
+	{
+		"date":"March 18",
+		"loc":"Kingston, NY",
+		"venue":"Tubby's w/ Ka Baird",
+		"link":"https://tubbyskingston.lyte.com/3707330"
+	},
+	{
+		"date":"March 19",
+		"loc":"Brooklyn, NY",
+		"venue":"Market Hotel w/ Nadah El-Shazly + Ka Baird",
+		"link":""
+	},
+	{
+		"date":"March 20",
+		"loc":"Philadelphia, PA",
+		"venue":"Solar Myth",
+		"link":""
+	},
+	{
+		"date":"March 21",
+		"loc":"Baltimore, MD",
+		"venue":"Ottobar w/ Ka Baird",
+		"link":"www.theottobar.com"
+	},
+	{
+		"date":"March 22 - 24",
+		"loc":"Knoxville, TN",
+		"venue":"Big Ears Festival",
+		"link":"www.bigearsfestival.com"
 	}
 	
 	
