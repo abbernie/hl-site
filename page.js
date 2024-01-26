069c7b194fb14160408e3231c21b1d97fa03ce26
Andrew <andrewbbernstein@gmail.com>
2024-01-26 16:50:17 +0100

    Update page.js

--- page.js
+++ page.js
@@ -1,54 +1,7 @@
 
 var dates = [
 
-		{
-		"date":"August 31",
-		"loc":"London, United Kingdom",
-		"venue":"Studio 9294",
-		"link":"http://studio9294.co.uk/"
-	},
-		{
-		"date":"September 1",
-		"loc":"Dorset, United Kingdom",
-		"venue":"End of the Road Festival",
-		"link":"https://endoftheroadfestival.com/"
-	},
-	{
-		"date":"September 2",
-		"loc":"Birmingham, United Kingdom",
-		"venue":"Supersonic Festival",
-		"link":"https://supersonicfestival.com/"
-	},
-	{
-		"date":"September 9",
-		"loc":"Corato, Italy",
-		"venue":"Secolare Festival",
-		"link":"https://dice.fm/event/l5ovr-secolare-festival-2023-full-pass-9th-sep-agriturismo-san-giuseppe-corato-tickets?lng=en"
-	},
-		{
-		"date":"October 6",
-		"loc":"Barreiro, Portugal",
-		"venue":"Out.Fest",
-		"link":"https://outfest.pt/eng/"
-	},
-		{
-		"date":"October 13",
-		"loc":"Bydgoscz, Poland",
-		"venue":"Fonomo Music & Film Festival",
-		"link":"https://www.instagram.com/fonomo_festival/?hl=en"
-	},
-	{
-		"date":"October 14",
-		"loc":"Belgrade, Serbia",
-		"venue":"Changeover Festival",
-		"link":"http://changeoverfestival.com/"
-	},
-	{
-		"date":"November 10 - 12",
-		"loc":"Wels, Austria",
-		"venue":"Music Unlimited",
-		"link":"https://www.waschaecht.at/music-unlimited/de"
-	},
+		
 	{
 		"date":"March 12",
 		"loc":"Minneapolis, MN",
@@ -89,7 +42,7 @@ var dates = [
 		"date":"March 18",
 		"loc":"Kingston, NY",
 		"venue":"Tubby's w/ Ka Baird",
-		"link":"https://tubbyskingston.lyte.com/3707330"
+		"link":"https://link.dice.fm/Te75a6208893"
 	},
 	{
 		"date":"March 19",
